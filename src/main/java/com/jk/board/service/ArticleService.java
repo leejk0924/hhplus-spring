5c4964aa6da2db0a5cf9f76813537e9b0b5051bf
jk <akdirdl@gmail.com>
2025-06-17 01:23:28 +0900

    refactor: ArticleDto에 List 변환 메서드 추가 및 ArticleService 로직 정리
    - DTO 변환 책임을 서비스로부터 분리

--- src/main/java/com/jk/board/service/ArticleService.java
+++ src/main/java/com/jk/board/service/ArticleService.java
@@ -1,20 +1,17 @@
 package com.jk.board.service;
 
-import com.jk.board.domain.Article;
 import com.jk.board.dto.ArticleDto;
 import com.jk.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
     public List<ArticleDto> searchArticles() {
-        List<Article> articles = articleRepository.findAll();
-        return articles.stream().map(ArticleDto::from).toList();
+        return ArticleDto.from(articleRepository.findAll());
     }
 }
