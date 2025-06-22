0316c92691ce67f28534b06410698e4b0c169414
jk <akdirdl@gmail.com>
2025-06-22 10:40:42 +0900

    refactor: select all 시 발생하는 N+1 문제 개선

--- src/main/java/com/jk/board/repository/ArticleRepository.java
+++ src/main/java/com/jk/board/repository/ArticleRepository.java
@@ -1,11 +1,17 @@
 package com.jk.board.repository;
 
 import com.jk.board.domain.Article;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @NonNull
+    @EntityGraph(attributePaths = {"userId"})
+    List<Article> findAll();
     Optional<Article> findByIdWithUser(Long id);
 }
 
