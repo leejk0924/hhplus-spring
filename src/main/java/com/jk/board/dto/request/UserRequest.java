485986a6d15e24550f4c6ab8c1f8ab3c0f65324e
jk <akdirdl@gmail.com>
2025-06-26 00:47:09 +0900

    feat: #8 - 회원가입 시 패스워드 조건 추가
    - 기존의 최소 8자 이상, 15자 이하이며, 알파벳 대소문자, 숫자에서 특수문자도 포함 가능하도록 기능 추가

--- src/main/java/com/jk/board/dto/request/UserRequest.java
+++ src/main/java/com/jk/board/dto/request/UserRequest.java
@@ -11,7 +11,7 @@ record UserRequest (
         String username,
         @NotBlank(message = "password 을 입력해주세요.")
         @Size(min = 8, max = 15,  message = "password 는 8자 이상 15자 이하입니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "password 는 영문 대소문자와 숫자만 사용할 수 있습니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9\\W_]+$", message = "password 는 영문 대소문자와 숫자만 사용할 수 있습니다.")
         String password
 ) {
 }
